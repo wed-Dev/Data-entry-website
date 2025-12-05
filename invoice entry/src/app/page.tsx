ef60a2a5c6322f662d210a6d391db3399de7e8c2
Abb561 <nijatshehzadi597@gmail.com>
2025-12-06 02:06:30 +0500

    Simplify root redirect

--- invoice entry/src/app/page.tsx
+++ invoice entry/src/app/page.tsx
@@ -1,12 +1,5 @@
 import { redirect } from 'next/navigation'
-import { getServerSession } from 'next-auth/next'
 
-export default async function RootRedirect() {
-  const session = await getServerSession()
-  
-  if (session) {
-    redirect('/dashboard')
-  } else {
-    redirect('/auth/login')
-  }
+export default function RootRedirect() {
+  redirect('/auth/login')
 }
